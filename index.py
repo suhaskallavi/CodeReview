e9ceebd00f387ed0b1733abf728b3bbe38a8cd19
suhas <sd35@pdx.edu>
2024-03-12 16:59:44 -0700

    Added search_results function

--- index.py
+++ index.py
@@ -23,3 +23,20 @@ class Index(MethodView):
             return self.search_results(search_query, tmdb_api_key)
         else:
             return self.default_index_page(tmdb_api_key)
+    
+
+    def search_results(self, search_query, tmdb_api_key):
+        tmdb_url = f'https://api.themoviedb.org/3/search/tv?api_key={tmdb_api_key}&query={search_query}'
+        tmdb_response = requests.get(tmdb_url)
+        
+        if tmdb_response.status_code == 200:
+            tmdb_data = tmdb_response.json()
+            filtered_shows = self.filter_tmdb_data(tmdb_data)
+            if filtered_shows:
+                platforms_logos = self.get_platforms_logos(search_query)
+                return render_template('result.html', shows=filtered_shows, platforms_logos=platforms_logos)
+            else:
+                return self.default_index_page_alert('Sorry, no show found with this name. Please try again!')
+        else:
+            return self.default_index_page()
+
